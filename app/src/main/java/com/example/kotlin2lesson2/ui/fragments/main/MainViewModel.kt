fe5d948fb43f328856dabd94f7eea98b963797d5
Emir <emirsagyndykov931@gmail.com>
2022-01-15 19:48:19 +0600

    я все стянул

--- app/src/main/java/com/example/kotlin2lesson2/ui/fragments/main/MainViewModel.kt
+++ app/src/main/java/com/example/kotlin2lesson2/ui/fragments/main/MainViewModel.kt
@@ -2,5 +2,5 @@ package com.example.kotlin2lesson2.ui.fragments.main
 
 import com.example.kotlin2lesson2.common.base.BaseViewModel
 
-class MainViewModel() : BaseViewModel() {
+class MainViewModel : BaseViewModel() {
 }
