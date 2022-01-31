650facc88cdd4d7022e32ecb540e6bc23bb7576e
Emir <emirsagyndykov931@gmail.com>
2022-01-31 18:31:50 +0600

    add search

--- domain/src/main/java/com/example/domain/repository/topNews/TopNewsRepository.kt
+++ domain/src/main/java/com/example/domain/repository/topNews/TopNewsRepository.kt
@@ -1,10 +1,5 @@
 package com.example.domain.repository.topNews
 
-import com.example.kotlin2lesson2.common.resource.Resource
-import com.example.kotlin2lesson2.data.remote.dtos.news.NewsResponseDto
-import com.example.kotlin2lesson2.domain.model.News
-import com.example.kotlin2lesson2.domain.model.Source
-
 import com.example.domain.common.resource.Resource
 import com.example.domain.model.News
 import kotlinx.coroutines.flow.Flow
